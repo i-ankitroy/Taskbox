ad377af9f62e4df2100b02c394c5525e3f76985f
Ankit Roy <ankitkumarroy48@gmail.com>
2026-07-26 20:48:52 +0530

    Add update and delete routes - full CRUD complete

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -15,6 +15,7 @@ const createTask = async (req,res) => {
     }
 };
 
+// GET REQUEST
 const getTasks = async (req,res) => {
     try{
         const tasks = await Task.find().sort({ createdAt: -1 });
@@ -25,6 +26,7 @@ const getTasks = async (req,res) => {
     }
 };
 
+// GET REQUEST WITH PARAMETER
 const getTaskById = async (req,res) => {
     try{
         const task = await Task.findById(req.params.id);
@@ -40,4 +42,42 @@ const getTaskById = async (req,res) => {
     }
 };
 
-module.exports = { createTask, getTasks, getTaskById };
+//UPDATE REQUEST
+const updateTask = async (req,res) => {
+    try{
+        const {title, description, completed} = req.body;
+    
+        const task = await Task.findById(req.params.id);
+
+        if (!task) {
+        return res.status(404).json({message:"Task not found"});
+        }
+        
+        task.title = title !== undefined ? title : task.title;
+        task.description = description !== undefined ? description : task.description;
+        task.completed = completed !== undefined ? completed : task.completed;
+
+        const updatedTask = await task.save();
+        res.status(200).json(updatedTask);
+    } catch (error) {
+        res.status(500).json({message: 'Server Error', error: error.message});
+    }
+};
+
+const deleteTask = async (req,res) => {
+    try{
+        const task = await Task.findById(req.params.id);
+
+        if (!task) {
+            return res.status(404).json({message:'Task Not Found'});
+        }
+
+        await task.deleteOne();
+        res.status(200).json({message:'Task Deleted Successfully'});
+    } catch (error) {
+        res.status(500).json({message: 'Server Error', error: error.message});
+    }
+};
+
+
+module.exports = { createTask, getTasks, getTaskById,updateTask, deleteTask };
